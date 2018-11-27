9f96a0b2c1265cad954031ed06c025da8441b692
Burr Sutter <burrsutter@gmail.com>
2018-11-26 19:36:32 -0500

    nodeport instead of port-forward

--- minikube_jaeger.sh
+++ minikube_jaeger.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# kubectl -n istio-system port-forward $(kubectl get pod -n istio-system -l app=jaeger -o jsonpath='{.items[0].metadata.name}') 16686:16686
+
 # open localhost:16686
 # https://www.jaegertracing.io/docs/1.6/getting-started/
 
-kubectl -n istio-system port-forward $(kubectl get pod -n istio-system -l app=jaeger -o jsonpath='{.items[0].metadata.name}') 16686:16686
+# OR
+
+kubectl patch service/jaeger-query -p '{"spec":{"type":"NodePort"}}' -n istio-system
+
+open http://$(minikube ip):$(kubectl get svc jaeger-query -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
