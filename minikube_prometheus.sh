152df9ba469a253e52dd1461e8f05a018f5a3948
Burr Sutter <burrsutter@gmail.com>
2019-02-07 12:02:33 -0500

    nodeport instead of port-forward

--- minikube_prometheus.sh
+++ minikube_prometheus.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# localhost:9090
+kubectl patch service/prometheus -p '{"spec":{"type":"NodePort"}}' -n istio-system
 
-kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}') 9090:9090
+open http://$(minikube ip):$(kubectl get svc prometheus -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
+
+# kubectl -n istio-system port-forward $(kubectl -n istio-system get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}') 9090:9090
 
