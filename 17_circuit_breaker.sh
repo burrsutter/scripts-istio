98c51363e9677f38452e70dc45fc4ce18dcaa009
Burr Sutter <burrsutter@gmail.com>
2019-03-14 12:46:16 -0400

    create to replace

--- 17_circuit_breaker.sh
+++ 17_circuit_breaker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-command1="kubectl create -f ../istio-tutorial/istiofiles/destination-rule-recommendation_cb_policy_version_v2.yml -n tutorial"
+command1="kubectl replace -f ../istio-tutorial/istiofiles/destination-rule-recommendation_cb_policy_version_v2.yml -n tutorial"
 echo $command1
 $command1
 
