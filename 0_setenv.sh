e91a4538c1456c8d038deb80d87c38e04118a89e
Burr Sutter <burrsutter@gmail.com>
2019-04-07 09:11:02 -0400

    updated to istio 1.1.1

--- 0_setenv.sh
+++ 0_setenv.sh
@@ -2,12 +2,12 @@
 
 echo 'correct usage is source 0_setenv.sh'
 
-export MINIKUBE_HOME=/Users/burrsutter/minikube_0.35.0/bin;
+export MINIKUBE_HOME=/Users/burrsutter/minikube_1.0.0/bin;
 export PATH=$MINIKUBE_HOME:$PATH
 export KUBECONFIG=$MINIKUBE_HOME/.kube/config:$MINIKUBE_HOME/.kube/kubconfig2
 export KUBE_EDITOR="code -w"
 
-export ISTIO_HOME=/Users/burrsutter/minikube_0.35.0/scripts-istio/istio-1.0.5
+export ISTIO_HOME=/Users/burrsutter/$MINIKUBE_HOME/scripts-istio/istio-1.1.1
 export PATH=$ISTIO_HOME/bin:$PATH
 
 # https://github.com/GoogleContainerTools/krew
--- 2_install_istio.sh
+++ 2_install_istio.sh
@@ -1,13 +1,16 @@
-curl -L https://github.com/istio/istio/releases/download/1.0.5/istio-1.0.5-osx.tar.gz | tar xz
+#!/bin/bash
+
+# curl -L https://github.com/istio/istio/releases/download/1.0.5/istio-1.0.5-osx.tar.gz | tar xz
 
 # curl -L https://github.com/istio/istio/releases/download/1.0.2/istio-1.0.2-osx.tar.gz | tar xz
-
 # curl -L https://github.com/istio/istio/releases/download/1.0.2/istio-1.0.2-linux.tar.gz | tar xz
 
 #cd istio-1.0.2
 
-cd istio-1.0.5
+curl -L https://github.com/istio/istio/releases/download/1.1.1/istio-1.1.1-osx.tar.gz | tar xz
 
-kubectl apply -f install/kubernetes/helm/istio/templates/crds.yaml
+cd istio-1.1.1
+
+kubectl apply -f install/kubernetes/helm/istio-init/files/crd-11.yaml
 
 kubectl apply -f install/kubernetes/istio-demo.yaml
