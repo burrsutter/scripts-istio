fe7df7f10b2a98b78513e57b8f5e17d37bc6817b
Burr Sutter <burrsutter@gmail.com>
2019-04-11 22:43:57 -0400

    removing pool_ejector demo

--- 20_pool_ejector.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-command1="kubectl replace -f ../istio-tutorial/istiofiles/destination-rule-recommendation_cb_policy_pool_ejection.yml -n tutorial"
-echo $command1
-$command1
--- 21_retry.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-command1="kubectl replace -f ../istio-tutorial/istiofiles/virtual-service-recommendation-v1_and_v2_retry.yml -n tutorial"
-echo $command1
-$command1
-
