1a66f92d59a492426f2fc4cc6437080bdfccff1b
Burr Sutter <burrsutter@gmail.com>
2019-04-11 23:26:32 -0400

    oc to kubectl

--- mtls_demo/1_exec_preference.sh
+++ mtls_demo/1_exec_preference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PREFPOD=$(oc get pod -n tutorial -l app=preference -o \
+PREFPOD=$(kubectl get pod -n tutorial -l app=preference -o \
 'jsonpath={.items[0].metadata.name}')
 
 kubectl exec -it $PREFPOD -n tutorial -c istio-proxy -- sudo tcpdump -A -s 0 'tcp port 8080 and (((ip[2:2]-((ip[0]&0xf)<<2))-((tcp[12]&0xf0)>>2))!= 0)'
