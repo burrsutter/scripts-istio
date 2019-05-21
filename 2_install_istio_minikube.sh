cc6cc398df8c8fad31595e7f94485538be4a02eb
Burr Sutter <burrsutter@gmail.com>
2019-05-21 19:00:57 -0400

    more tweaks

--- 2_install_istio_minikube.sh
+++ 2_install_istio_minikube.sh
@@ -4,9 +4,9 @@ curl -L https://github.com/istio/istio/releases/download/1.1.1/istio-1.1.1-osx.t
 
 cd istio-1.1.1
 
-kubectl apply -f install/kubernetes/helm/istio-init/files/crd-11.yaml
+kubectl create -f install/kubernetes/helm/istio-init/files/crd-11.yaml
 
-kubectl apply -f install/kubernetes/istio-demo.yaml
+kubectl create -f install/kubernetes/istio-demo.yaml
 
 kubectl patch service/grafana -p '{"spec":{"type":"NodePort"}}' -n istio-system
 kubectl patch service/prometheus -p '{"spec":{"type":"NodePort"}}' -n istio-system
--- 2_install_istio_minishift.sh
+++ 2_install_istio_minishift.sh
@@ -8,9 +8,9 @@ kubectl create -f install/kubernetes/helm/istio-init/files/crd-11.yaml
 
 kubectl create -f install/kubernetes/istio-demo.yaml
 
-oc expose svc istio-ingressgateway
-oc expose svc grafana
-oc expose svc prometheus
-oc expose svc tracing
-oc expose service kiali --path=/kiali
+oc expose svc grafana -n istio-system
+oc expose svc prometheus -n istio-system
+oc expose svc tracing -n istio-system
+oc expose service kiali --path=/kiali -n istio-system
+oc expose svc istio-ingressgateway -n istio-system
 oc adm policy add-cluster-role-to-user admin system:serviceaccount:istio-system:kiali-service-account -z default
