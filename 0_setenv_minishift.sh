c263f0cfa59c0797cb1666038686574eba475683
Burr Sutter <burrsutter@gmail.com>
2019-05-21 18:48:58 -0400

    fixing an errant minikube in the minishift script

--- 0_setenv_minishift.sh
+++ 0_setenv_minishift.sh
@@ -4,7 +4,7 @@ echo 'correct usage is source 0_setenv.sh'
 
 export MINISHIFT_HOME=/Users/burrsutter/minishift_1.33.0/bin;
 export PATH=$MINISHIFT_HOME:$PATH
-export KUBECONFIG=$MINISHIFT_HOME/.kube/config:$MINIKUBE_HOME/.kube/kubconfig2
+export KUBECONFIG=$MINISHIFT_HOME/.kube/config:$MINISHIFT_HOME/.kube/kubconfig2
 export KUBE_EDITOR="code -w"
 
 export ISTIO_HOME=$MINISHIFT_HOME/scripts-istio/istio-1.1.1
