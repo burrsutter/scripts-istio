90b0baa06c5821188bbee94b30588dbceb20bcfd
Burr Sutter <burrsutter@gmail.com>
2019-05-21 19:35:40 -0400

    minikube vs minishift

--- 3_install_microservices_remote_minikube.sh
+++ 3_install_microservices_remote_minikube.sh
@@ -2,9 +2,7 @@
 kubectl create namespace tutorial
 kubens tutorial
 
-# on minishift
-oc adm policy add-scc-to-user privileged -z default -n tutorial
-kubectl label set istio-injection=enabled 
+# kubectl label set istio-injection=enabled 
 
 kubectl apply -f <(istioctl kube-inject -f ../istio-tutorial/customer/kubernetes/Deployment.remote.yml) -n tutorial
 kubectl apply -f <(istioctl kube-inject -f ../istio-tutorial/preference/kubernetes/Deployment.remote.yml) -n tutorial
