0d8c5da3a56edbb76502634b8d22cb0031b8e01c
Burr Sutter <burrsutter@gmail.com>
2019-05-21 21:59:21 -0400

    adding skipTests

--- 4_deploy_recommendationv2.sh
+++ 4_deploy_recommendationv2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ../istio-tutorial/recommendation/java/quarkus
 
-mvn clean package
+mvn clean package -DskipTests
 
 docker build -t example/recommendation:v2 .
 
-kubectl apply -f <(istioctl kube-inject -f ../../kubernetes/Deployment-v2.yml) -n tutorial
+kubectl apply -f <(istioctl kube-inject -f ../istio-tutorial/recommendation/kubernetes/Deployment-v2.yml) -n tutorial
