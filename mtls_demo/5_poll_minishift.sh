9bc2820515cc4c04b7c19b7deaddcb10b49747c9
Burr Sutter <burrsutter@gmail.com>
2019-06-21 15:27:56 -0400

    tweaked the poller

--- mtls_demo/5_poll_minishift.sh
+++ mtls_demo/5_poll_minishift.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+# INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 
-GATEWAY_URL=$(minishift --profile istio-ms ip):$INGRESS_PORT
+# GATEWAY_URL=$(minishift --profile istio-ms ip):$INGRESS_PORT
+
+# while true
+# do curl http://${GATEWAY_URL}/
+#sleep .3
+# done
+
+IP=$(minishift --profile istio-ms ip)
 
 while true
-do curl http://${GATEWAY_URL}/
+do curl istio-ingressgateway-istio-system.$IP.nip.io/customer
 sleep .3
 done
 
