5ee8edf147fb9335147bb4718d7907342f50866f
Burr Sutter <burrsutter@gmail.com>
2019-08-27 11:43:41 -0400

    removed deletion of gateway

--- mtls_demo/clean.sh
+++ mtls_demo/clean.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-kubectl delete -f ../../istio-tutorial/istiofiles/gateway-customer.yml
 kubectl delete -n tutorial -f ../../istio-tutorial/istiofiles/destination-rule-tls.yml
 kubectl delete -n tutorial -f ../../istio-tutorial/istiofiles/authentication-enable-tls.yml
