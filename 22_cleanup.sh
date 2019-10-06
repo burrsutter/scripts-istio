50ef14605f39617a88dd9ad6203a859b549ef310
Burr Sutter <burrsutter@gmail.com>
2019-10-06 19:17:48 -0400

    using the auto-injection

--- 22_cleanup.sh
+++ 22_cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kubectl scale deployment recommendation-v2 --replicas=1 -n tutorial
+kubectl delete deployment recommendation-v2
 kubectl delete pod -l app=recommendation,version=v2
 kubectl delete virtualservice/recommendation
 kubectl delete destinationrule/recommendation
