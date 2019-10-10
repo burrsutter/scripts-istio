4fc081e250019ea87d8c6a80d655e651e3c6a00b
Burr Sutter <burrsutter@gmail.com>
2019-10-10 16:20:41 -0400

    better ACL whitelist example

---
+++ white_list/curl_nginx2customer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NGINXPOD=$(kubectl get pod -n egresstest -l app=nginx -o \
+'jsonpath={.items[0].metadata.name}')
+
+kubectl exec -it $NGINXPOD -n egresstest -- curl customer.tutorial.svc.cluster.local:8080
