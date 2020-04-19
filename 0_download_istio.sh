f1ac535f52f1b20fb3b46aee482a09cf81826d19
Burr Sutter <burrsutter@gmail.com>
2020-04-19 09:37:51 -0400

    updating to istio 1.4.6 and kube 1.17.4

---
+++ 0_download_istio.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -L https://github.com/istio/istio/releases/download/1.4.6/istio-1.4.6-osx.tar.gz | tar xz
--- 0_setenv_tryopenshift.sh
+++ 0_setenv_tryopenshift.sh
@@ -5,9 +5,8 @@ echo 'correct usage is source 0_setenv.sh'
 export PROJECT_HOME=/Users/burrsutter/tryopenshift;
 export PATH=$PROJECT_HOME/bin:$PATH
 # export KUBECONFIG=$PROJECT_HOME/.kube/config
-export KUBECONFIG=$PROJECT_HOME/july15v1/auth/kubeconfig
+export KUBECONFIG=$PROJECT_HOME/azr-apr18/auth/kubeconfig
 export KUBE_EDITOR="code -w"
 
-export ISTIO_HOME=$PROJECT_HOME/scripts-istio/istio-1.3.0
+export ISTIO_HOME=$PROJECT_HOME/scripts-istio/istio-1.4.6
 export PATH=$ISTIO_HOME/bin:$PATH
-
