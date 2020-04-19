f0c4343c2efb5fef74c46bbe20732fc59915b9e6
Burr Sutter <burrsutter@gmail.com>
2020-04-19 12:43:22 -0400

    updating istio version

--- 0_setenv_minikube.sh
+++ 0_setenv_minikube.sh
@@ -2,13 +2,13 @@
 
 echo 'correct usage is source 0_setenv.sh'
 
-export MINIKUBE_HOME=/Users/burrsutter/11steps;
+export MINIKUBE_HOME=/Users/burrsutter/devnation;
 # this is where the minikube executable is
 export PATH=$MINIKUBE_HOME/bin:$PATH
 export KUBECONFIG=$MINIKUBE_HOME/.kube/config
 export KUBE_EDITOR="code -w"
 
-export ISTIO_HOME=$MINIKUBE_HOME/scripts-istio/istio-1.3.0
+export ISTIO_HOME=$MINIKUBE_HOME/scripts-istio/istio-1.4.6
 export PATH=$ISTIO_HOME/bin:$PATH
 
 echo 'the following after minikube as started'
