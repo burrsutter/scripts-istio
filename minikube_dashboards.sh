bb99c3beea24401a3518f78fdb039d1cec6e1a6e
burrsutter <burrsutter@gmail.com>
2020-07-17 07:39:36 -0400

    fixed profile name

--- minikube_dashboards.sh
+++ minikube_dashboards.sh
@@ -3,19 +3,19 @@
 
 kubectl patch service/grafana -p '{"spec":{"type":"NodePort"}}' -n istio-system
 
-open http://$(minikube --profile istio-mk ip):$(kubectl get svc grafana -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
+open http://$(minikube ip):$(kubectl get svc grafana -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
 
 
 kubectl patch service/jaeger-query -p '{"spec":{"type":"NodePort"}}' -n istio-system
 
-open http://$(minikube --profile istio-mk ip):$(kubectl get svc jaeger-query -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
+open http://$(minikube ip):$(kubectl get svc jaeger-query -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
 
 
 kubectl patch service/prometheus -p '{"spec":{"type":"NodePort"}}' -n istio-system
 
-open http://$(minikube --profile istio-mk ip):$(kubectl get svc prometheus -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
+open http://$(minikube ip):$(kubectl get svc prometheus -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
 
 
 kubectl patch service/kiali -p '{"spec":{"type":"NodePort"}}' -n istio-system
 
-open http://$(minikube --profile istio-mk ip):$(kubectl get svc kiali -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')/kiali
+open http://$(minikube ip):$(kubectl get svc kiali -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')/kiali
