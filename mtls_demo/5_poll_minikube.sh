60ae544c485a5a66e236d4ab2ccc955547452fdf
burrsutter <burrsutter@gmail.com>
2020-07-17 09:07:14 -0400

    fixing profile

--- mtls_demo/5_poll_minikube.sh
+++ mtls_demo/5_poll_minikube.sh
@@ -2,7 +2,7 @@
 
 INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 
-GATEWAY_URL=$(minikube -p istio-mk ip):$INGRESS_PORT
+GATEWAY_URL=$(minikube ip):$INGRESS_PORT
 
 while true
 do curl http://${GATEWAY_URL}/customer
